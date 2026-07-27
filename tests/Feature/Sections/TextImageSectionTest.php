f16070aa2425b4047c975a6f4695519056220dbd
Arnaud De Burchgraeve <99794461+arnaud-dbu@users.noreply.github.com>
2026-07-27 11:09:51 +0200

    feat(textImage): anker en omgekeerde kolomvolgorde als losse argumenten
    De volgorde beeld/tekst hing tot nu toe aan de background-toggle. De
    servicepagina wisselt af zonder achtergrondvlak, dus die twee moeten los.
    Zonder de nieuwe argumenten rendert het partial ongewijzigd.

--- tests/Feature/Sections/TextImageSectionTest.php
+++ tests/Feature/Sections/TextImageSectionTest.php
@@ -33,4 +33,50 @@ class TextImageSectionTest extends SectionTestCase
 
         $this->assertStringNotContainsString('text-image--background', $html);
     }
+
+    public function test_renders_the_anchor_as_a_section_id(): void
+    {
+        $html = $this->render('{{ partial src="sections/textImage" anchor="advies" }}', [
+            'title' => 'Advies op maat',
+        ]);
+
+        $this->assertStringContainsString('id="advies"', $html);
+    }
+
+    public function test_omits_the_id_attribute_without_an_anchor(): void
+    {
+        $html = $this->render('{{ partial src="sections/textImage" }}', ['title' => 'Advies op maat']);
+
+        $this->assertStringNotContainsString('id="', $html);
+    }
+
+    public function test_puts_the_text_column_last_by_default(): void
+    {
+        $html = $this->render('{{ partial src="sections/textImage" }}', ['title' => 'Advies op maat']);
+
+        $this->assertStringContainsString('order-last', $html);
+    }
+
+    public function test_text_first_moves_the_text_column_ahead_of_the_image(): void
+    {
+        $html = $this->render('{{ partial src="sections/textImage" text_first="true" }}', [
+            'title' => 'Vakkundige installatie',
+        ]);
+
+        $this->assertStringNotContainsString('order-last', $html);
+    }
+
+    public function test_background_still_wins_over_text_first(): void
+    {
+        // `background` zette de tekstkolom al vooraan, mét lichte kaart. Dat gedrag
+        // mag niet veranderen nu `text_first` hetzelfde effect langs een andere weg
+        // bereikt: de acht bestaande aanroepers geven `text_first` nooit mee.
+        $html = $this->render('{{ partial src="sections/textImage" text_first="true" }}', [
+            'title' => 'Drie lokale verkooppunten',
+            'background' => true,
+        ]);
+
+        $this->assertStringContainsString('bg-light card-padding', $html);
+        $this->assertStringNotContainsString('order-last', $html);
+    }
 }
